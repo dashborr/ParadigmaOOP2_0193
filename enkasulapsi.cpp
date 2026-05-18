9e70c76b9612b05f3174f0ece583f998a0dbafa3
dashborr <danisarfas7@gmail.com>
2026-05-18 14:54:33 +0700

    menambahkan class remotelampu

--- enkasulapsi.cpp
+++ enkasulapsi.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
+class remotelampu{
+
+    private:
+    string saklarno[5];
+    public:
+    void setSaklarno(int i,string value){
+        saklarno[i] = value;
+    }
+
+    string getSaklarno(int i){
+        return saklarno[i];
+    }
+};
