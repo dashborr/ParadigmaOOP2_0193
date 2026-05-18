ed98c5fc65bcb8fb0439f94d342eb9b7207acbb4
dashborr <danisarfas7@gmail.com>
2026-05-18 15:12:29 +0700

    menambahkan public

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -2,3 +2,13 @@
 using namespace std;
 
 class orang
+{
+public:
+    int umur;
+    orang(int pUmur) : 
+    umur(pUmur)
+    {
+        cout << " orang di buat dengan umur " << umur << "\n"
+             << endl;
+    }
+};
