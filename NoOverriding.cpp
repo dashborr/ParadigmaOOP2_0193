5ae73714e2320b90960bcebf5601cca8093cfe02
dashborr <danisarfas7@gmail.com>
2026-05-18 15:05:01 +0700

    menambahkan baseclass

--- NoOverriding.cpp
+++ NoOverriding.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 
 using namespace std;
+
+class baseclass{
+    public:
+     virtual void perkenalan(){
+        cout << "hallo Saya function dari base class";
+
+        }
+};
